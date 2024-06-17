78998f5bbfc7511ac46e029f5d3afa8b3acd1929
JUNG SUNG HYUN <ssjung99@gmail.com>
2024-06-18 04:55:15 +0900

    requestBody 받기 위한처리 수정함.

--- src/main/java/kp/cmsc/common/config/WebConfig.java
+++ src/main/java/kp/cmsc/common/config/WebConfig.java
@@ -1,10 +1,19 @@
 package kp.cmsc.common.config;
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.util.ContentCachingRequestWrapper;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import kp.cmsc.common.interceptor.LoggerInterceptor;
 
 @Configuration
@@ -14,9 +23,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")//특정메소드 호출
                 .allowedOriginPatterns("http://localhost:3000")
-//                .allowedOriginPatterns("/**")
                 .allowedMethods("GET", "POST")
-                .allowCredentials(false);//원하는 시간만큼 pre-flight 리퀘스트를 캐싱
+                .allowCredentials(true);//원하는 시간만큼 pre-flight 리퀘스트를 캐싱
     }
 
     @Autowired
@@ -26,6 +34,18 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loggerInterceptor).addPathPatterns("/**");
     }
-
-
+    /**
+     *requestBody르 받기 위해 반드시 필요함.
+     * @return
+     */
+    @Bean
+    public OncePerRequestFilter requestCachingFilter() {
+        return new OncePerRequestFilter() {
+            @Override
+            protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+                HttpServletRequest cachedRequest = new ContentCachingRequestWrapper(request);
+                filterChain.doFilter(cachedRequest, response);
+            }
+        };
+    }
 }
